053b374fa563e251e008d5159565004defa27baa
M1829JS <142667393+M1829JS@users.noreply.github.com>
2023-09-11 19:12:59 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let password = document.getElementById("password");
 let login = document.getElementById("login");
+let create = false;
+let log = document.getElementsByClassName('log');
 function sign() {
   if (password = login) {
     SubmitEvent.apply;
@@ -7,4 +9,11 @@ function sign() {
     document.getElementById("login").value = "";
     alert("please enter the correct password");
   };
+};
+function created() {
+  create = true;
+};
+if (create = true) {
+  document.getElementsByClassName('log').innerHTML = "";
+  log.innerHTML = "";
 };
