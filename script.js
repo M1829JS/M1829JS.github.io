f720ca25e017d008aac2354a4501bc529427d796
M1829JS <142667393+M1829JS@users.noreply.github.com>
2023-09-13 10:43:10 +0300

    initial commit

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ function sign() {
     alert("please enter the correct password");
   };
 };
+let selectedFile = document.getElementById("formFile").files[0];
+let inputelement = document.getElementById("formFile");
+inputElement.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  const fileList = this.files; /* now you can work with the file list */
+};
+if (inputelement >= 1) {
+
+}
+const numFiles = fileList.length;
+let dropbox;
+
+dropbox = document.getElementById("dropbox");
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+};
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+};
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const dt = e.dataTransfer;
+  const files = dt.files;
+
+  handleFiles(files);
+};
